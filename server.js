ed2d2369a80c69e33a2fd2593be6d0aca2c18363
Paul <paul@dedsec.dedsec>
2022-03-03 17:25:48 +0530

    Test Deploy

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
